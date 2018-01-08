8577706b96edfbe145cc379dce297f863224cf6d
emanuelevecchio <34943796+emanuelevecchio@users.noreply.github.com>
2018-01-08 17:57:08 +0100

    Created governance01-runbook-tagging_addrequiredrgtags.ps1 in AzureAutomation.

---
+++ azure/governance01-runbook-tagging_addrequiredrgtags.ps1
@@ -0,0 +1,53 @@
+############################################################################################
+#                                                                                          #
+# This script add every *required* ResourceGroup tags into each resource contained in it.  #
+# It does not ovverride existing tags.                                                     #
+#                                                                                          #
+############################################################################################
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+$requiredTags = @("bl", "env", "owner", "region", "svc")
+
+#$resGroups = Get-AzureRmResourceGroup -Name (Find-AzureRmResourceGroup -Tag @{Name = 'owner'; Value = 'emanuele.vecchio@magnetimarelli.com'}).Name
+$resGroups = Get-AzureRmResourceGroup
+foreach ($resGroup in $resGroups) {
+    if ($resGroup.Tags -ne $null) {
+		$resources = $resGroup | Find-AzureRmResource
+		foreach ($resource in $resources)
+		{
+            Write-Output $resource.Name
+            
+            $resourcetagsource = (Get-AzureRmResource -ResourceId $resource.ResourceId).Tags
+            if ($resourcetagsource -eq $null) {
+                $resourcetagsource =  @()
+            }
+            $resourcetags = $resourcetagsource
+            foreach ($tag in $resGroup.Tags.GetEnumerator())
+            {
+                if ($requiredTags.contains($tag.Name) -And !$resourcetagsource.ContainsKey($tag.Name)) { $resourcetags.Add($tag.Name,$tag.Value) }
+            }
+            
+			Set-AzureRmResource -Tag $resourcetags -ResourceId $resource.ResourceId -Force
+		}
+	}
+}
